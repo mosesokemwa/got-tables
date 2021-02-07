dbd925c11533197ffe788acc664444362111d9b5
Moses Okemwa <13944912+mosesokemwa@users.noreply.github.com>
2021-02-07 12:36:20 +0300

    loading characters data in index template
    Signed-off-by: Moses Okemwa <13944912+mosesokemwa@users.noreply.github.com>>

--- app/routes.py
+++ app/routes.py
@@ -1,28 +1,28 @@
 from os import environ
 from flask import render_template, request, current_app as app
-from .services import api_data, characters
+from .services import characters_query, houses_query, api_data
 
 
+CHARACTERS_ENDPOINT = "/characters/"
+HOUSES_ENDPOINT = "/houses/"
 
-CHARACTERS_ENDPOINT = '/characters/'
 
-@app.route('/',  methods=['GET'])
+@app.route("/", methods=["GET"])
 def index():
-    people = characters(f'{CHARACTERS_ENDPOINT}?page=1')
-    return render_template('index.html', people=people)
+    people = characters_query(f"{CHARACTERS_ENDPOINT}?page=1")
+    return render_template("index.html", people=people)
 
 
-@app.route('/details/<id_>', methods=['GET'])
+@app.route("/houses", methods=["GET"])
+def houses():
+    h = houses_query(f"{HOUSES_ENDPOINT}?page=1")
+    return render_template("houses.html", houses=h)
+
+
+@app.route("/details/<id_>", methods=["GET"])
 def details(id_):
-    character_detail = api_data(f'{CHARACTERS_ENDPOINT}{id_}')
-
-    # new_details = {
-    #     'name': details['name'],
-    #     "height": details["height"],
-    #     "mass": details["mass"],
-    #     "hair_color": details["hair_color"],
-    #     "skin_color": details["skin_color"],
-    #     "eye_color": details["eye_color"],
-    #     "birth_year": details["birth_year"]
-    # }
-    return render_template('single.html', details=character_detail, id=id_)
+    character_detail = api_data(f"{CHARACTERS_ENDPOINT}{id_}")
+    print(character_detail)
+    for k in character_detail:
+        print(k)
+    return render_template("single.html", details=character_detail, id=id_)
--- app/services.py
+++ app/services.py
@@ -1,28 +1,52 @@
 import json
-from typing import  AnyStr, List
+from typing import List, Union
+
 import requests
 
+PEOPLE_ENDPOINT = "/characters/"
+GOT_ENDPOINT = "https://anapioficeandfire.com/api/"
 
-PEOPLE_ENDPOINT = '/characters/'
-GOT_ENDPOINT='https://anapioficeandfire.com/api/'
 
 def api_data(endpoint) -> str:
-    res = requests.get(f'{GOT_ENDPOINT}{endpoint}' )
+    res = requests.get(f"{GOT_ENDPOINT}{endpoint}")
     return res.json()
 
-def get_list_data(list) -> List[AnyStr]:
+
+def get_list_data(list, key) -> List[Union[int, str, float]]:
     info = []
     for _ in range(len(list)):
         r = requests.get(list[_])
-        info.append(r.json()['name'])
+        info.append(r.json()[key])
     return info
 
 
-def characters(endpoint) -> str:
+def houses_query(endpoint) -> str:
+    """Returns GOT Houses data
+    Args:
+        endpoint: Str
+    Result:
+        data: Dict
+    """
     data = api_data(endpoint)
     for _ in range(len(data)):
-        books = get_list_data(data[_]['books'])
-        if not data[_]['name']:
-            data[_]['name'] = 'No name'
-        data[_]['books'] = books
+        swornMembers = get_list_data(data[_]["swornMembers"], "name")
+        if not data[_]["name"]:
+            data[_]["name"] = "No name"
+        data[_]["swornMembers"] = swornMembers
+    return data
+
+
+def characters_query(endpoint) -> str:
+    """Returns GOT Characters data
+    Args:
+        endpoint: Str
+    Result:
+        data: Dict
+    """
+    data = api_data(endpoint)
+    for _ in range(len(data)):
+        books = get_list_data(data[_]["books"], "name")
+        if not data[_]["name"]:
+            data[_]["name"] = "No name"
+        data[_]["books"] = books
     return data
